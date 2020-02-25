4602e9ab35c8a009607b159ead18acac1afd15eb
Victoria Khan <supernovaedaisy@gmail.com>
2020-02-25 21:23:35 +0900

    V2

--- run_analysis.R
+++ run_analysis.R
@@ -3,8 +3,9 @@ library(readr)
 library(dplyr)
 library(magrittr)
 library(data.table)
+library(stringr)
 
-
+# Merges the training and the test sets to create one data set.
 ## Creating a test dataset by merging three datasets from the folder "test"
 subject_test <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
                            "UCI HAR Dataset/test/subject_test.txt", sep = ""), 
@@ -15,8 +16,6 @@ X_test <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
 Y_test <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
                                  "UCI HAR Dataset/test/y_test.txt", sep = ""), 
                            quote="\"", comment.char="", stringsAsFactors=FALSE)
-Test = do.call(cbind, list(subject_test, Y_test, X_test)) 
-
 
 ## Creating a training dataset by merging three datasets from the folder "train"
 subject_train <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
@@ -28,8 +27,19 @@ X_train <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
 Y_train <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
                            "UCI HAR Dataset/train/y_train.txt", sep = ""), 
                      quote="\"", comment.char="", stringsAsFactors=FALSE)
+
+
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+features <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/",
+                             "features.txt", sep=""), quote="\"", comment.char="")
+
+colnames(X_test) = features$V2
+colnames(X_train) = features$V2
+Test = do.call(cbind, list(subject_test, Y_test, X_test)) 
 Train = do.call(cbind, list(subject_test, Y_test, X_test)) 
 Activities = rbind(Test, Train)
+Activities[, c(1,2,grep("[Mm]ean|[Ss]td", names(Activities)))] -> subset_activities
+
 
 
 
