fd57561d977c943ceaf0b456f3a3b498ff5e4f28
Victoria Khan <supernovaedaisy@gmail.com>
2020-02-25 17:47:44 +0900

    Change of the underlying data

--- run_analysis.R
+++ run_analysis.R
@@ -4,27 +4,32 @@ library(dplyr)
 library(magrittr)
 library(data.table)
 
-#Loading Testing data
-testlist =list.files(paste("C:/Users/vkhan/Desktop//Coursera; R programming/",
-                           "Getting and cleaning data/Week 4/Programming Assignment/",
-                           "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Inertial Signals", sep=""))
-setwd(paste("C:/Users/vkhan/Desktop//Coursera; R programming/",
-            "Getting and cleaning data/Week 4/Programming Assignment/",
-            "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Inertial Signals", sep=""))
 
-Test = rbindlist(sapply(testlist, fread, simplify = FALSE), use.names = TRUE, idcol = "FileName")
-sum(is.na(Train))  
+## Creating a test dataset by merging three datasets from the folder "test"
+subject_test <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
+                           "UCI HAR Dataset/test/subject_test.txt", sep = ""), 
+                           quote="\"", comment.char="", stringsAsFactors=FALSE)
+X_test <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
+                                 "UCI HAR Dataset/test/X_test.txt", sep = ""), 
+                           quote="\"", comment.char="", stringsAsFactors=FALSE)
+Y_test <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
+                                 "UCI HAR Dataset/test/y_test.txt", sep = ""), 
+                           quote="\"", comment.char="", stringsAsFactors=FALSE)
+Test = do.call(cbind, list(subject_test, Y_test, X_test)) 
 
-#loading Training data
-trainlist =list.files(paste("C:/Users/vkhan/Desktop//Coursera; R programming/",
-                            "Getting and cleaning data/Week 4/Programming Assignment/",
-                            "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Inertial Signals", sep=""))
-setwd(paste("C:/Users/vkhan/Desktop/Coursera; R programming/",
-            "Getting and cleaning data/Week 4/Programming Assignment/",
-            "getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Inertial Signals", sep=""))
 
-Train = rbindlist(sapply(trainlist, fread, simplify = FALSE), use.names = TRUE, idcol = "FileName")
-sum(is.na(Train))                              
+## Creating a training dataset by merging three datasets from the folder "train"
+subject_train <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
+                                 "UCI HAR Dataset/train/subject_train.txt", sep = ""), 
+                           quote="\"", comment.char="", stringsAsFactors=FALSE)
+X_train <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
+                           "UCI HAR Dataset/train/X_train.txt", sep = ""), 
+                     quote="\"", comment.char="", stringsAsFactors=FALSE)
+Y_train <- read.table(paste("./getdata_projectfiles_UCI HAR Dataset/",
+                           "UCI HAR Dataset/train/y_train.txt", sep = ""), 
+                     quote="\"", comment.char="", stringsAsFactors=FALSE)
+Train = do.call(cbind, list(subject_test, Y_test, X_test)) 
+Activities = rbind(Test, Train)
 
 
 
